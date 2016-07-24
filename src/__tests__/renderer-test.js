0f905ccbd6f7db8a7414854736c5b63244b32ad2
Theodor Tonum <theodor@tonum.no>
2016-07-24 21:55:32 +0200

    Use correct coords and sizes for rendering

--- src/__tests__/renderer-test.js
+++ src/__tests__/renderer-test.js
@@ -10,6 +10,13 @@ jest.unmock('../renderer');
 jest.unmock('../util/sprite-name-from-gid');
 jest.unmock('../__fixtures__/map-1.tmx.js');
 
+interface Component2D {
+  x: number;
+  y: number;
+  w: number;
+  h: number,
+}
+
 describe('Renderer', () => {
   let renderer;
 
@@ -20,11 +27,49 @@ describe('Renderer', () => {
     });
   });
 
-  it('should be able to render a map', () => {
+  it('should be able to render a map to the canvas', () => {
     renderer.render();
     expect(Crafty('Tile, Sprite').length).toBe(16);
     expect(Crafty('TileSprite_1').length).toBe(11);
     expect(Crafty('TileSprite_6').length).toBe(3);
     expect(Crafty('TileSprite_7').length).toBe(2);
+  });
+
+  it('should render the top right row correctly', () => {
+    renderer.render();
+    const tiles: Component2D[] = Crafty('2D').get();
+
+    tiles.forEach((tile) => {
+      expect(tile.w).toBe(256);
+      expect(tile.h).toBe(128);
+    });
+
+    expect(tiles[0].x).toBe(512);
+    expect(tiles[1].x).toBe(640);
+    expect(tiles[2].x).toBe(768);
+    expect(tiles[3].x).toBe(896);
+    expect(tiles[0].y).toBe(64);
+    expect(tiles[1].y).toBe(128);
+    expect(tiles[2].y).toBe(192);
+    expect(tiles[3].y).toBe(256);
+  });
+
+  it('should render the bottom left row correctly', () => {
+    renderer.render();
+    const tiles: Component2D[] = Crafty('2D').get();
+
+    tiles.forEach((tile) => {
+      expect(tile.w).toBe(256);
+      expect(tile.h).toBe(128);
+    });
+
+    expect(tiles[12].x).toBe(128);
+    expect(tiles[13].x).toBe(256);
+    expect(tiles[14].x).toBe(384);
+    expect(tiles[15].x).toBe(512);
+    expect(tiles[12].y).toBe(256);
+    expect(tiles[13].y).toBe(320);
+    expect(tiles[14].y).toBe(384);
+    expect(tiles[15].y).toBe(448);
   });
 });
