43b68007dcc9d3e1e15d9e5c5541431c58a38543
Theodor Tonum <theodor@tonum.no>
2016-07-25 00:37:09 +0200

    Enable rendering of object groups

--- src/__tests__/renderer-test.js
+++ src/__tests__/renderer-test.js
@@ -3,12 +3,14 @@
 /* global Crafty */
 import 'craftyjs/dist/crafty.js'; // TODO: Fix this ugliness
 import Renderer from '../renderer';
-import loadMap from '../__fixtures__/map-1.tmx.js';
+import loadTileMap from '../__fixtures__/map-1.tmx.js';
+import loadObjectMap from '../__fixtures__/object-example.tmx.js';
 
 jest.unmock('tmx-parser');
 jest.unmock('../renderer');
 jest.unmock('../util/sprite-name-from-gid');
 jest.unmock('../__fixtures__/map-1.tmx.js');
+jest.unmock('../__fixtures__/object-example.tmx.js');
 
 interface Component2D {
   x: number;
@@ -20,56 +22,105 @@ interface Component2D {
 describe('Renderer', () => {
   let renderer;
 
-  beforeEach((done) => {
-    loadMap().then((map) => {
-      renderer = new Renderer(map);
-      done();
+  afterEach(() => {
+    Crafty.stop();
+    Crafty('*').each(function destroy() { this.destroy(); });
+  });
+
+  describe('basic tile rendering', () => {
+    beforeEach((done) => {
+      loadTileMap().then((map) => {
+        renderer = new Renderer(map);
+        done();
+      });
+    });
+
+    it('should be able to render a map to the canvas', () => {
+      renderer.render();
+      expect(Crafty('Tile, Sprite').length).toBe(16);
+      expect(Crafty('TileSprite_1').length).toBe(11);
+      expect(Crafty('TileSprite_6').length).toBe(3);
+      expect(Crafty('TileSprite_7').length).toBe(2);
+    });
+
+    it('should render the top right row correctly', () => {
+      renderer.render();
+      const tiles: Component2D[] = Crafty('2D').get();
+
+      tiles.forEach((tile) => {
+        expect(tile.w).toBe(256);
+        expect(tile.h).toBe(128);
+      });
+
+      expect(tiles[0].x).toBe(512);
+      expect(tiles[1].x).toBe(640);
+      expect(tiles[2].x).toBe(768);
+      expect(tiles[3].x).toBe(896);
+      expect(tiles[0].y).toBe(64);
+      expect(tiles[1].y).toBe(128);
+      expect(tiles[2].y).toBe(192);
+      expect(tiles[3].y).toBe(256);
+    });
+
+    it('should render the bottom left row correctly', () => {
+      renderer.render();
+      const tiles: Component2D[] = Crafty('2D').get();
+
+      tiles.forEach((tile) => {
+        expect(tile.w).toBe(256);
+        expect(tile.h).toBe(128);
+      });
+
+      expect(tiles[12].x).toBe(128);
+      expect(tiles[13].x).toBe(256);
+      expect(tiles[14].x).toBe(384);
+      expect(tiles[15].x).toBe(512);
+      expect(tiles[12].y).toBe(256);
+      expect(tiles[13].y).toBe(320);
+      expect(tiles[14].y).toBe(384);
+      expect(tiles[15].y).toBe(448);
     });
   });
 
-  it('should be able to render a map to the canvas', () => {
-    renderer.render();
-    expect(Crafty('Tile, Sprite').length).toBe(16);
-    expect(Crafty('TileSprite_1').length).toBe(11);
-    expect(Crafty('TileSprite_6').length).toBe(3);
-    expect(Crafty('TileSprite_7').length).toBe(2);
-  });
-
-  it('should render the top right row correctly', () => {
-    renderer.render();
-    const tiles: Component2D[] = Crafty('2D').get();
-
-    tiles.forEach((tile) => {
-      expect(tile.w).toBe(256);
-      expect(tile.h).toBe(128);
+  describe('Rendering objects', () => {
+    beforeEach((done) => {
+      loadObjectMap().then((map) => {
+        renderer = new Renderer(map);
+        done();
+      });
     });
 
-    expect(tiles[0].x).toBe(512);
-    expect(tiles[1].x).toBe(640);
-    expect(tiles[2].x).toBe(768);
-    expect(tiles[3].x).toBe(896);
-    expect(tiles[0].y).toBe(64);
-    expect(tiles[1].y).toBe(128);
-    expect(tiles[2].y).toBe(192);
-    expect(tiles[3].y).toBe(256);
-  });
-
-  it('should render the bottom left row correctly', () => {
-    renderer.render();
-    const tiles: Component2D[] = Crafty('2D').get();
-
-    tiles.forEach((tile) => {
-      expect(tile.w).toBe(256);
-      expect(tile.h).toBe(128);
+    it('should detect and add the object as an entity', () => {
+      renderer.render();
+      expect(Crafty('Obj Tree').length).toBe(1);
     });
 
-    expect(tiles[12].x).toBe(128);
-    expect(tiles[13].x).toBe(256);
-    expect(tiles[14].x).toBe(384);
-    expect(tiles[15].x).toBe(512);
-    expect(tiles[12].y).toBe(256);
-    expect(tiles[13].y).toBe(320);
-    expect(tiles[14].y).toBe(384);
-    expect(tiles[15].y).toBe(448);
+    it('should render the tree with the correct sprite', () => {
+      renderer.render();
+      expect(Crafty('Tree TileSprite_50').length).toBe(1);
+    });
+
+    it('should size and place the entity correctly', () => {
+      renderer.render();
+      const entity = Crafty('Tree 2D').get(0);
+      expect(entity.x).toBe(512); // 2 tile widths
+      expect(entity.y).toBe(448); // 3 tile height + padding
+      expect(entity.w).toBe(256);
+      expect(entity.h).toBe(256);
+    });
+
+    it('should annotate for easier debugging', () => {
+      renderer.render();
+      expect(Crafty('Tree').get(0)._entityName).toBe('This is a tree'); // eslint-disable-line
+    });
+
+    it('should handle custom properties', () => {
+      renderer.render();
+      const tree = Crafty('Tree').get(0);
+      expect(tree.attr('color')).toBe('green');
+      expect(tree.attr('growing')).toBe(true);
+      expect(tree.attr('height')).toBe(2);
+      expect(tree.attr('pi')).toBe(3.14);
+    });
   });
 });
--- src/renderer.js
+++ src/renderer.js
@@ -21,6 +21,22 @@ type Vector2dMutableType = Vector2dType | {
   add(vec: Vector2dType): Vector2dMutableType;
 }
 
+type TmxObjectType = {
+  name: string;
+  type: ?string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rotation: number;
+  properties: Object;
+  gid: number;
+  visible: boolean;
+  ellipse: boolean;
+  polygon: ?boolean;
+  polyline: ?boolean;
+}
+
 export default class Renderer {
   map: MapType;
   tileSize: Vector2dType;
@@ -59,32 +75,83 @@ export default class Renderer {
 
         this.renderTile(tile.gid, x, y);
       });
+    } else if (layer.type === 'object') {
+      layer.objects.forEach(this.renderObject.bind(this));
     }
   }
 
   renderTile(gid: number, x: number, y: number) {
-    const deltaX = x - y;
-    const deltaY = x + y;
+    const position = this.translateCoordinates({ x, y });
 
-    const position = new Crafty.math.Vector2D(deltaX, deltaY)
-                                    .multiply(this.tileSize)
-                                    .scale(1 / 2)
-                                    .add(this.origo);
+    this.renderSprite({
+      components: ['Tile'],
+      x: position.x,
+      y: position.y,
+      width: this.tileSize.x,
+      height: this.tileSize.y,
+      gid,
+    });
+  }
 
+  renderObject(object: TmxObjectType) {
+    const position = this.translateCoordinates(
+      this.normalizeCoordinates(object)
+    );
+
+    this.renderSprite({
+      components: ['Obj', object.type],
+      x: position.x,
+      y: position.y,
+      width: object.width,
+      height: object.height,
+      gid: object.gid,
+    }).setName(object.name).attr(object.properties);
+  }
+
+  renderSprite(data: {
+    components: Array<?string>,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    gid: number
+  }): any {
     const componentList = [
       '2D',
-      'Tile',
-      spriteNameFromGid(gid),
+      spriteNameFromGid(data.gid),
     ];
+
+    data.components.forEach((component) => {
+      if (component) componentList.push(component);
+    });
 
     // TODO: Add some kind of test for this
     if (isCanvasSupported()) componentList.push('Canvas');
 
-    Crafty.e(...componentList).attr({
-      x: position.x,
-      y: position.y,
-      w: this.tileSize.x,
-      h: this.tileSize.y,
+    return Crafty.e(...componentList).attr({
+      x: data.x,
+      y: data.y,
+      w: data.width,
+      h: data.height,
     });
+  }
+
+  // Converts a vector of small coordinates to screen coordinates
+  translateCoordinates(vector: Vector2dType): Vector2dType {
+    const { x, y } = vector;
+    const deltaX = x - y;
+    const deltaY = x + y;
+
+    return new Crafty.math.Vector2D(deltaX, deltaY)
+                  .multiply(this.tileSize)
+                  .scale(1 / 2)
+                  .add(this.origo);
+  }
+
+  // Takes a vector of large coordinates (like [256, 128]) and
+  // converts them to small coordinates (like [2, 1])
+  normalizeCoordinates(vector: Vector2dType): Vector2dType {
+    const { x, y } = vector;
+    return new Crafty.math.Vector2D(x, y).scale(1 / this.tileSize.y);
   }
 }
